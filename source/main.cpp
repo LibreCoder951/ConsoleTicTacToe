c0a577a054c1f249a9c499a05b321e3ef695280c
LibreCoder951 <axhd1808@gmail.com>
2020-03-22 13:10:02 +0100

    Added O player

--- source/main.cpp
+++ source/main.cpp
@@ -16,11 +16,11 @@ namespace tictactoe {
 	int ycoordInput;
 
 	void gameExplanation() {
-		std::cout << "You're the \"X\" and you must win against the \"O\" in a game of tic-tac-toe." << std::endl << std::endl;
+		std::cout << "Du bist das Kreuz und musst gegen den Kreis gewinnen" << std::endl << std::endl;
 	}
 
 	void drawBoard() {
-		std::cout << "Current board: " << std::endl;
+		std::cout << "Das jetzige Spielfeld: " << std::endl;
 		std::cout << "    0   1   2" << std::endl;
 		std::cout << "  -------------" << std::endl;
 		std::cout << "0 | " << upperLeftCorner << " | " << upperMiddle << " | " << upperRightCorner << " |" << std::endl;
@@ -32,13 +32,13 @@ namespace tictactoe {
 	}
 
 	void getAndProcessXINput() {
-		// Get the input
-		std::cout << "X, what should be your X coordinate?";
+		// Fragen
+		std::cout << "Kreuz, was ist deine X Koordinate?";
 		std::cin >> xcoordInput;
-		std::cout << "What should be your Y coordinate?";
+		std::cout << "Und was ist deine Y Koordinate?";
 		std::cin >> ycoordInput;
 
-		// Process it
+		// Verarbeitung
 		if (xcoordInput == 0 && ycoordInput == 0) {
 			upperLeftCorner = 'X';
 		} else if (xcoordInput == 1 && ycoordInput == 0) {
@@ -58,27 +58,53 @@ namespace tictactoe {
 		} else if (xcoordInput == 2 && ycoordInput == 2) {
 			downRightCorner = 'X';
 		} else {
-			std::cout << "That is not a valid position." << std::endl << "Try again." << std::endl;
+			std::cout << "Das ist keine gültige Position." << std::endl << "Versuch es noch einmal." << std::endl;
+			tictactoe::getAndProcessXINput();
+		}
+	}
+	void getAndProcessOINput() {
+		// Fragen
+		std::cout << "Kreis, was ist deine X Koordinate?";
+		std::cin >> xcoordInput;
+		std::cout << "Und was ist deine Y Koordinate?";
+		std::cin >> ycoordInput;
+
+		// Verarbeitung
+		if (xcoordInput == 0 && ycoordInput == 0) {
+			upperLeftCorner = 'O';
+		} else if (xcoordInput == 1 && ycoordInput == 0) {
+			upperMiddle = 'O';
+		} else if (xcoordInput == 2 && ycoordInput == 0) {
+			upperRightCorner = 'O';
+		} else if (xcoordInput == 0 && ycoordInput == 1) {
+			middleLeft = 'O';
+		} else if (xcoordInput == 1 && ycoordInput == 1) {
+			middle = 'O';
+		} else if (xcoordInput == 2 && ycoordInput == 1) {
+			middleRight = 'O';
+		} else if (xcoordInput == 0 && ycoordInput == 2) {
+			downLeftCorner = 'O';
+		} else if (xcoordInput == 1 && ycoordInput == 2) {
+			downMiddle = 'O';
+		} else if (xcoordInput == 2 && ycoordInput == 2) {
+			downRightCorner = 'O';
+		} else {
+			std::cout << "Das ist keine gültige Position." << std::endl << "Versuch es noch einmal." << std::endl;
 			tictactoe::getAndProcessXINput();
 		}
 	}
 }
 
-void printLicenseInfo() {
-	std::cout << "CLI-TTT  Copyright (C) 2020  LibreCoder951" << std::endl;
-	std::cout << "This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'." << std::endl;
-	std::cout << "This is free software, and you are welcome to redistribute it" << std::endl;
-	std::cout << "under certain conditions; type `show c' for details." << std::endl << std::endl << std::endl;
-}
-
 int main() {
-	printLicenseInfo();
 	tictactoe::gameExplanation();
 	std::cin;
 	for (;;) {
 		system("cls");
 		tictactoe::drawBoard();
 		tictactoe::getAndProcessXINput();
+		system("cls");
+		tictactoe::drawBoard();
+		tictactoe::getAndProcessOINput();
 	}
 	return 0;
 }
