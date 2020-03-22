24c761eeefeb64ec451ab6e9814f8c4777be4a57
LibreCoder951 <axhd1808@gmail.com>
2020-03-22 08:46:51 +0100

    Added a basic c++ project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tic tac toe
+
+Not my game idea, but now it's in your commandline.
---
+++ source/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "CLI-TTT  Copyright (C) 2020  LibreCoder951" << endl << "This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'." << "This is free software, and you are welcome to redistribute it" << endl << "under certain conditions; type `show c' for details." << endl;
+	return 0;
+}
